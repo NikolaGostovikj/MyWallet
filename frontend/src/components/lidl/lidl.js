29f44bcc4652a34e4d54eeb9fa6f634fa8e00b6a
Nikola Gostovikj <nikolagostovic8@gmail.com>
2025-08-09 21:21:14 +0200

    Small add stores

---
+++ frontend/src/components/lidl/lidl.js
@@ -0,0 +1,13 @@
+import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
+import './lidlCss.css'
+
+function Lidl(){
+
+return(<div>
+
+</div>);
+
+}
+
+export default Lidl;
---
+++ frontend/src/components/mercator/mercator.js
@@ -0,0 +1,13 @@
+import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
+import './mercatorCss.css';
+
+function Mercator(){
+
+    return(<div>
+
+    </div>)
+    
+}
+
+export default Mercator;
