6530c46334f419b5124fd3672599a2782b131f08
Nikola Gostovikj <nikolagostovic8@gmail.com>
2025-08-15 16:30:16 +0200

    Fixed the admin Page UI, now just to manage roles

--- frontend/src/components/adminPage/adminPage.js
+++ frontend/src/components/adminPage/adminPage.js
@@ -3,13 +3,15 @@ import { useNavigate } from "react-router-dom";
 import './adminPageCss.css';
 function AdmingPage(){
     const navigate = useNavigate();
-    return(<div className="header">
+    return(
+    <div className="admin-page">
+    <div className="header">
         <div className="holder">
             <button className="button" onClick={()=>navigate("/manageUsers")}>Manage Users</button>
             <button className="button" onClick={()=>navigate("/userRoles")}>Manage User Roles</button>
             <button className="button" onClick={()=>navigate("/bank")}>Back</button>
         </div>
-        
+    </div>
     </div>)
 }
 
